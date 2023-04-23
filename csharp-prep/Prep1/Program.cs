ce11ba6cf8e1d61ff58ec145de330bd0d53a56a3
owensjakopo <jak22001@byui.edu>
2023-04-23 07:33:19 +0200

    Edited variable names

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,14 +6,16 @@ class Program
     {
         // Ask the users for their first name.
         Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine();
+        string first = Console.ReadLine();
 
         //Now ask for their last name.
         Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine();
+        string last = Console.ReadLine();
+
+        //print a blank line.
         Console.WriteLine("");
 
         //Now print out their full name.
-        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.Write($"Your name is {last}, {first} {last}.");
     }
 }
