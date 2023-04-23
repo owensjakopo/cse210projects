1c9265ada172b6aa96887508edec6bf4496a3e94
owensjakopo <jak22001@byui.edu>
2023-04-23 07:25:55 +0200

    Finished prep 1 project

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask the users for their first name.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        //Now ask for their last name.
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("");
+
+        //Now print out their full name.
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
