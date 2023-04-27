dba7f2b202e2a569a9f82546fe4946cd38028f9f
owensjakopo <jak22001@byui.edu>
2023-04-27 05:57:36 +0200

    Fixed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage: ");
+        string number = Console.ReadLine();
+        Console.WriteLine("");
+
+        int grade = int.Parse(number);
+        string letter = "";
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You have passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better Luck next time!");
+        }
     }
 }
