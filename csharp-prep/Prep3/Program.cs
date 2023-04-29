150d87984458197b87e7c4e0ae52dc6062827552
owensjakopo <jak22001@byui.edu>
2023-04-29 16:32:52 +0200

    Fixed prep3 with the help of the sample solution

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = Console.ReadLine();
+        int yourNumber = -1;
+        while (yourNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            yourNumber = int.Parse(Console.ReadLine());
+            if (yourNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (yourNumber < magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (yourNumber > magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
